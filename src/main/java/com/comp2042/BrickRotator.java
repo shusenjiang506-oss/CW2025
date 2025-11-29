1085e731d58d6c2d8ea514554952647fecf2a731
shusenjiang <shusenjiang506@gmail.com>
2025-11-29 20:33:02 +0800

    Added null safety checks

--- src/main/java/com/comp2042/BrickRotator.java
+++ src/main/java/com/comp2042/BrickRotator.java
@@ -7,13 +7,36 @@ public class BrickRotator {
     private Brick brick;
     private int currentShape = 0;
 
+    /*
     public NextShapeInfo getNextShape() {
         int nextShape = currentShape;
         nextShape = (++nextShape) % brick.getShapeMatrix().size();
         return new NextShapeInfo(brick.getShapeMatrix().get(nextShape), nextShape);
     }
+        */
+
+    public NextShapeInfo getNextShape() {
+        // Fixed: Added null check to prevent NullPointerException
+        if (brick == null) {
+            throw new IllegalStateException("No brick has been set");
+        }
+        int nextShape = currentShape;
+        nextShape = (++nextShape) % brick.getShapeMatrix().size();
+        return new NextShapeInfo(brick.getShapeMatrix().get(nextShape), nextShape);
+    }
+
+
+    /*
+    public int[][] getCurrentShape() {
+        return brick.getShapeMatrix().get(currentShape);
+    }
+    */
 
     public int[][] getCurrentShape() {
+        // Fixed: Added null check to prevent NullPointerException
+        if (brick == null) {
+            throw new IllegalStateException("No brick has been set");
+        }
         return brick.getShapeMatrix().get(currentShape);
     }
 
@@ -21,7 +44,18 @@ public class BrickRotator {
         this.currentShape = currentShape;
     }
 
+    /*
     public void setBrick(Brick brick) {
+        this.brick = brick;
+        currentShape = 0;
+    }
+        */
+
+    public void setBrick(Brick brick) {
+        // Fixed: Added null validation to prevent setting null brick
+        if (brick == null) {
+            throw new IllegalArgumentException("Brick cannot be null");
+        }
         this.brick = brick;
         currentShape = 0;
     }
